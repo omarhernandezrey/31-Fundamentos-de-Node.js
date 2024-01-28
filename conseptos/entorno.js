d70c96e57769ebda3e62c6fca122811de57f0d49
OmarHernandezRey <hernandezreyomar@gmail.com>
2024-01-28 16:09:58 -0500

    Clase 5 / 31 Configurar las variables de entorno en Node.js

---
+++ conseptos/entorno.js
@@ -0,0 +1,15 @@
+/* 31 Configurar las variables de entorno en Node.js */
+
+// Definición de la variable saludo con el valor 'hola'
+let saludo = 'hola';
+// Imprime el valor de la variable saludo en la consola
+console.log(saludo); // hola
+
+// Asignación de valores a las variables nombre y web usando variables de entorno de Node.js
+let nombre = process.env.NOMBRE || 'Sin nombre';
+let web = process.env.WEB || 'No tengo web';
+
+// Imprime un saludo usando la variable nombre en la consola
+console.log('Hola ' + nombre); // Hola Sin nombre
+// Imprime la dirección web usando la variable web en la consola
+console.log('Mi web es ' + web); // Mi web es No tengo web
